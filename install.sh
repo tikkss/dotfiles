2dab934e84ed9138e74542835a3d5821ffd90dd7
tikkss <tsuto.katsube@gmail.com>
2018-05-29 11:42:51 +0900

    Add window operation to .ideavimrc

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ dotfiles_base="$(cd $(dirname "$0") && pwd)"
 ln -snfv $dotfiles_base/.config/fish/config.fish ~/.config/fish/config.fish
 ln -snfv $dotfiles_base/.config/git/ignore ~/.config/git/ignore
 ln -snfv $dotfiles_base/.gitconfig ~/.gitconfig
+ln -snfv $dotfiles_base/.ideavimrc ~/.ideavimrc
 
 # for WSL
 if [[ `uname -a` =~ Linux && `uname -a` =~ Microsoft ]]; then
