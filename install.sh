02b42fa8595caad8c1755025e82bd457963fd207
tikkss <tsuto.katsube@gmail.com>
2018-06-01 16:01:16 +0900

    Revert "Add git information to fish"
    This reverts commit f02936e547bae69f04fed3c69f2988267f5a4808.
    
    fisher のテーマ `omf/theme-agnoster` をインストールすれば Git
    の情報が表示されるようになったので revert した。
    
    出来るだけ設定を書かないようにする fish way の考え方でいく。
    
    なお `omf/theme-agnoster` は Powerline フォントを利用するので、別途
    Powerline フォントのインストールと、ターミナルのフォント設定が必要。

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 dotfiles_base="$(cd $(dirname "$0") && pwd)"
 
 ln -snfv $dotfiles_base/.config/fish/config.fish ~/.config/fish/config.fish
-ln -snfv $dotfiles_base/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 ln -snfv $dotfiles_base/.config/git/ignore ~/.config/git/ignore
 ln -snfv $dotfiles_base/.gitconfig ~/.gitconfig
 ln -snfv $dotfiles_base/.ideavimrc ~/.ideavimrc
