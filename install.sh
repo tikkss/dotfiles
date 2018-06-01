f02936e547bae69f04fed3c69f2988267f5a4808
tikkss <tsuto.katsube@gmail.com>
2018-06-01 14:24:45 +0900

    Add git information to fish
    [Git Information in Fish Shell’s Prompt — Martin
    Klepsch](https://www.martinklepsch.org/posts/git-prompt-for-fish-shell.html)

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 dotfiles_base="$(cd $(dirname "$0") && pwd)"
 
 ln -snfv $dotfiles_base/.config/fish/config.fish ~/.config/fish/config.fish
+ln -snfv $dotfiles_base/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 ln -snfv $dotfiles_base/.config/git/ignore ~/.config/git/ignore
 ln -snfv $dotfiles_base/.gitconfig ~/.gitconfig
 ln -snfv $dotfiles_base/.ideavimrc ~/.ideavimrc
