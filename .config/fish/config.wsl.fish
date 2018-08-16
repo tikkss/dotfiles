b91bed8965465868af42af09e3442ce82ee04a5e
tikkss <tsuto.katsube@gmail.com>
2018-08-16 17:05:09 +0900

    Fix default umask (permissions) is not applied on wsl
    /etc/wsl.conf に umount の設定をしても、 WSL の umount
    の設定には反映されない不具合がある。
    
    マウントされた Windowsのフォルダやファイルの umount 設定は正常である。
    
    回避策として、WSL 独自の config.fish に umount 設定を追加した。
    
    https://github.com/Microsoft/WSL/issues/352

--- .config/fish/config.wsl.fish
+++ .config/fish/config.wsl.fish
@@ -1,5 +1,10 @@
 set -x DOCKER_HOST 'tcp://0.0.0.0:2375'
 
+switch (umask)
+case 0000
+  umask 0022
+end
+
 function rubymine
   "/c/Program Files/JetBrains/RubyMine 2018.1.4/bin/rubymine64.exe" .
 end
