020575dd04f197b5dae0f583b92c2fcd1eb60307
tikkss <tsuto.katsube@gmail.com>
2018-08-16 17:04:15 +0900

    Update rubymine version

--- .config/fish/config.wsl.fish
+++ .config/fish/config.wsl.fish
@@ -1,6 +1,6 @@
 set -x DOCKER_HOST 'tcp://0.0.0.0:2375'
 
 function rubymine
-  "/c/Program Files/JetBrains/RubyMine 2018.1.3/bin/rubymine64.exe" .
+  "/c/Program Files/JetBrains/RubyMine 2018.1.4/bin/rubymine64.exe" .
 end
 
