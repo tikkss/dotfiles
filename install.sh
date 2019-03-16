dff82c85b2e318844363e8413da2471391eebc60
tikkss <tsuto.katsube@gmail.com>
2019-03-16 17:33:48 +0900

    Fix no such file or directory

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 dotfiles_base="$(cd $(dirname "$0") && pwd)"
 
 ln -snfv $dotfiles_base/.config/fish/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/git
 ln -snfv $dotfiles_base/.config/git/ignore ~/.config/git/ignore
 ln -snfv $dotfiles_base/.gemrc ~/.gemrc
 ln -snfv $dotfiles_base/.gitconfig ~/.gitconfig
