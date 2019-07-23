b6748c05f6a97b5b6e5b19dcd8a9e1fec44a0a46
tikkss <tsuto.katsube@gmail.com>
2019-07-23 17:48:35 +0900

    Add beautiful diffs for tig

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -snfv $dotfiles_base/.config/git/ignore ~/.config/git/ignore
 ln -snfv $dotfiles_base/.gemrc ~/.gemrc
 ln -snfv $dotfiles_base/.gitconfig ~/.gitconfig
 ln -snfv $dotfiles_base/.ideavimrc ~/.ideavimrc
+ln -snfv $dotfiles_base/.tigrc ~/.tigrc
 ln -snfv $dotfiles_base/.tmux.conf ~/.tmux.conf
 ln -snfv $dotfiles_base/.vimrc ~/.vimrc
 
