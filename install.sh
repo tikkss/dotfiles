dee05c0ecf3237ac17b3753c904bf9b29ca6c6b7
tikkss <tsuto.katsube@gmail.com>
2020-09-17 09:58:24 +0900

    Limit memory and processors for WSL 2
    [WSL 2 はメモリ食い](https://dev.classmethod.jp/articles/how-to-setup-wsl2/)
    らしいので、[グローバル WSL オプション](https://docs.microsoft.com/ja-jp/windows/wsl/wsl-config#configure-global-options-with-wslconfig)
    を参考に、WSL 2 の上限 VM メモリ、Virtual プロセッサ数を設定した。

--- install.sh
+++ install.sh
@@ -41,6 +41,7 @@ if [[ `uname -a` =~ Linux && `uname -a` =~ Microsoft ]]; then
   ln -nfv $dotfiles_base/.config/memo/config.toml $userprofile/.config/memo/config.toml
   ln -nfv $dotfiles_base/.gitconfig $userprofile/.gitconfig
   ln -nfv $dotfiles_base/.ideavimrc $userprofile/.ideavimrc
+  ln -nfv $dotfiles_base/.wslconfig $userprofile/.wslconfig
   sudo ln -snfv /usr/bin/fish /usr/local/bin/fish
 
   # Build & link diff-highlight
