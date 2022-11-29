2554202fd7df278977402b3df10a2d638bf3bc40
tikkss <tsuto.katsube@gmail.com>
2022-11-29 11:14:16 +0900

    Introduce zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,9 @@ if [ -f ~/.config/fish/config.local.fish ]
   source ~/.config/fish/config.local.fish
 end
 
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
 starship init fish | source
+zoxide init fish | source
 
