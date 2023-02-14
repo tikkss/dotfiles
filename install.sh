843319f30dd4fb1e582e25c0e342279b32662f8e
tikkss <tsuto.katsube@gmail.com>
2023-02-14 14:56:28 +0900

    Install gh

--- install.sh
+++ install.sh
@@ -98,6 +98,19 @@ if [[ `uname -a` =~ Linux && `uname -a` =~ (M|m)icrosoft ]]; then
     ~/.fzf/install --all
   fi
 
+  # Install gh
+  gh --version > /dev/null 2>&1
+  if [ $? -eq 127 ]; then
+    type -p curl >/dev/null || sudo apt install curl -y
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+      && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+      && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+      && sudo apt update \
+      && sudo apt install gh -y
+  else
+    sudo apt update
+    sudo apt install gh -y
+  fi
 fi
 
 fish -c "fisher update"
